d560e6426cc89d8322196fc5c78765da851d2c55
Shafqat Ali <110333936+shafqatali06@users.noreply.github.com>
2025-01-03 15:56:52 +0500

    Update template.py

--- template.py
+++ template.py
@@ -2,7 +2,6 @@ import os
 from pathlib import Path
 import logging
 
-#logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = 'cnnClassifier'
